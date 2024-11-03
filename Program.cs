3467572dbaf4f4023b1acc914b203122cbb9415e
patty 💜 <87675545+p4tt7@users.noreply.github.com>
2024-11-03 19:11:46 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine());
+int contador = 1;
+int suma=0;
+while (contador<n*2){
+
+    if(contador%2!=0){
+        suma+=contador;
+        contador++;
+    }
+    else{
+        contador++;
+    }
+}
+Console.WriteLine(suma); 
